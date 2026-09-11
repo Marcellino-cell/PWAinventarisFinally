53bf39b84ac86152ff411a9351ce9642b9ad5afd
Marcellino-cell <fahrifajar379@gmail.com>
2026-09-11 20:37:09 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,216 +1,172 @@
-const CACHE_NAME =
-    "inventaris-ruangan-v6-0";
+const CACHE_NAME = "inventaris-it-v2";
 
 
-const APP_FILES = [
+const APP_SHELL = [
 
-    "./",
+  "./",
 
-    "./index.html",
+  "./index.html",
 
-    "./style.css",
+  "./style.css",
 
-    "./script.js",
+  "./script.js",
 
-    "./manifest.json"
+  "./manifest.json",
+
+  "./icons/icon-192.png",
+
+  "./icons/icon-512.png"
 
 ];
 
 
-/* =========================================================
-   INSTALL
-========================================================= */
-
 self.addEventListener(
-    "install",
-    event => {
+  "install",
+  (event) => {
 
-        event.waitUntil(
+    event.waitUntil(
 
-            caches
-                .open(
-                    CACHE_NAME
-                )
-                .then(
-                    cache =>
-                        cache.addAll(
-                            APP_FILES
-                        )
-                )
-                .then(
-                    () =>
-                        self.skipWaiting()
-                )
+      caches
 
-        );
+        .open(
+          CACHE_NAME
+        )
 
-    }
-);
-
-
-/* =========================================================
-   ACTIVATE
-========================================================= */
-
-self.addEventListener(
-    "activate",
-    event => {
-
-        event.waitUntil(
-
-            caches.keys()
-                .then(
-                    keys =>
-
-                        Promise.all(
-
-                            keys
-                                .filter(
-                                    key =>
-                                        key !==
-                                        CACHE_NAME
-                                )
-                                .map(
-                                    key =>
-                                        caches.delete(
-                                            key
-                                        )
-                                )
-
-                        )
-
-                )
-                .then(
-                    () =>
-                        self.clients.claim()
-                )
-
-        );
-
-    }
-);
-
-
-/* =========================================================
-   FETCH
-========================================================= */
-
-self.addEventListener(
-    "fetch",
-    event => {
-
-        if (
-            event.request.method !==
-            "GET"
-        ) {
-
-            return;
-
-        }
-
-
-        const url =
-            new URL(
-                event.request.url
-            );
-
-
-        /*
-           Jangan cache Firebase,
-           Google API, atau resource
-           eksternal.
-        */
-
-        if (
-            url.origin !==
-            self.location.origin
-        ) {
-
-            return;
-
-        }
-
-
-        event.respondWith(
-
-            caches.match(
-                event.request
+        .then(
+          (cache) =>
+            cache.addAll(
+              APP_SHELL
             )
-                .then(
-                    cached => {
+        )
 
-                        if (cached) {
+        .then(
+          () =>
+            self.skipWaiting()
+        )
 
-                            return cached;
+    );
 
-                        }
-
-
-                        return fetch(
-                            event.request
-                        )
-                            .then(
-                                response => {
-
-                                    if (
-                                        response &&
-                                        response.ok
-                                    ) {
-
-                                        const copy =
-                                            response.clone();
+  }
+);
 
 
-                                        caches
-                                            .open(
-                                                CACHE_NAME
-                                            )
-                                            .then(
-                                                cache =>
-                                                    cache.put(
-                                                        event.request,
-                                                        copy
-                                                    )
-                                            );
+self.addEventListener(
+  "activate",
+  (event) => {
 
-                                    }
+    event.waitUntil(
+
+      caches.keys().then(
+        (keys) =>
+          Promise.all(
+
+            keys
+
+              .filter(
+                (key) =>
+                  key !== CACHE_NAME
+              )
+
+              .map(
+                (key) =>
+                  caches.delete(
+                    key
+                  )
+              )
+
+          )
+
+      ).then(
+        () =>
+          self.clients.claim()
+      )
+
+    );
+
+  }
+);
 
 
-                                    return response;
+self.addEventListener(
+  "fetch",
+  (event) => {
 
-                                }
-                            )
-                            .catch(
-                                () => {
+    if (
+      event.request.method !==
+      "GET"
+    ) {
 
-                                    if (
-                                        event.request
-                                            .mode ===
-                                        "navigate"
-                                    ) {
-
-                                        return caches.match(
-                                            "./index.html"
-                                        );
-
-                                    }
-
-
-                                    return new Response(
-                                        "",
-                                        {
-                                            status:
-                                                503
-                                        }
-                                    );
-
-                                }
-                            );
-
-                    }
-                )
-
-        );
+      return;
 
     }
+
+
+    event.respondWith(
+
+      caches
+        .match(
+          event.request
+        )
+
+        .then(
+          (cached) => {
+
+            if (cached) {
+              return cached;
+            }
+
+
+            return fetch(
+              event.request
+            )
+
+              .then(
+                (response) => {
+
+                  if (
+                    response &&
+                    response.status ===
+                      200 &&
+                    response.type ===
+                      "basic"
+                  ) {
+
+                    const copy =
+                      response.clone();
+
+
+                    caches
+                      .open(
+                        CACHE_NAME
+                      )
+
+                      .then(
+                        (cache) =>
+                          cache.put(
+                            event.request,
+                            copy
+                          )
+                      );
+
+                  }
+
+
+                  return response;
+
+                }
+              )
+
+              .catch(
+                () =>
+                  caches.match(
+                    "./index.html"
+                  )
+              );
+
+          }
+        )
+
+    );
+
+  }
 );
